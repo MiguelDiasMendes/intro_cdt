cb01951a0d32e2178a9815b243c0c7f8a2e52d14
Aluno em vocacao Leandro <leandrocarmoxav@gmail.com>
2026-07-02 17:36:12 -0300

    upgrade
    aprendemos modulo4 sobres as tuplas, diconarios, listas

--- dicionario_mod04_MiguelDias.py
+++ dicionario_mod04_MiguelDias.py
@@ -3,22 +3,35 @@ print('-' *40)
 
 cardapio_semana = { 
 "segunda-feira": "Arroz com feijão",
+
 "terça-feira": "Strogonoff",
+
 "quarta-feira": "Feijoada",
+
 "quinta-feira": "Sopa",
+
 "sexta-feira": "Pizza",
+
 "sabado": "Churrasco",
-"domigo": "Macarrão"
+
+"domingo": "Macarrão"
+
 }
 
 nome_usuario = input('Digite seu nome para começar: \n')
-print(cardapio_semana)
+print(f'\nOlá, {nome_usuario}! Seja bem-vindo ao nosso cardápio da semana!\n')
+print('-' *40)
+print("Aqui você pode consultar o prato do dia de acordo com o dia da semana que você escolher.\n")   
+print(f'\n{cardapio_semana}\n')
 
-selecione_opcao = input('Selecione o dia da semana: \n')
+selecione_opcao = input('Selecione o dia da semana: \n').lower().strip()
 
 if selecione_opcao in cardapio_semana: 
     comida = cardapio_semana [selecione_opcao]
-    print(f"No {selecione_opcao}, o dia é de comer {comida}!")
+    print(f'\n{selecione_opcao}, o dia é de comer {comida}!\n')
 
 else:
-    print("Desculpe, não temos esse dia em nosso cardapio")
+    print("Desculpe, não temos esse dia em nosso cardapio, tente colocar o hífen (ex: segunda-feira).")
+
+deseja_prosseguir = input("Deseja prosseguir com seu pedido?:  ")
+print('\nVá para a aba de pagamentos\n')
